73c0b77c7fe184bc07857933f6413d1ae91eddf0
AnhDo173 <150677440+AnhDo173@users.noreply.github.com>
2026-02-23 15:33:31 +0000

    overspending detection + trends

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -33,7 +33,6 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
-
 // GET ALL TRANSACTIONS
 router.get("/", verifyToken, async (req, res) => {
   try {
@@ -107,11 +106,14 @@ router.get("/summary", verifyToken, async (req, res) => {
       }
     });
 
+    const overspending = expense > income * 0.9;
+
     res.json({
       period,
       income,
       expense,
       balance: income - expense,
+      overspending, 
       categoryBreakdown: categoryMap
     });
 
@@ -119,5 +121,57 @@ router.get("/summary", verifyToken, async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+router.get("/trends", verifyToken, async (req, res) => {
+  try {
+    const userId = req.user.uid;
+    const { period } = req.query;
+
+    const snapshot = await db
+      .collection("users")
+      .doc(userId)
+      .collection("transactions")
+      .get();
+
+    const trendMap = {};
+
+    snapshot.forEach(doc => {
+      const data = doc.data();
+      const date = data.createdAt.toDate();
+
+      let key;
+
+      if (period === "daily") {
+        key = date.toISOString().split("T")[0]; // YYYY-MM-DD
+      } else if (period === "weekly") {
+        const week = Math.ceil(date.getDate() / 7);
+        key = `${date.getFullYear()}-W${week}`;
+      } else if (period === "monthly") {
+        key = `${date.getFullYear()}-${date.getMonth() + 1}`;
+      } else {
+        key = date.toISOString().split("T")[0];
+      }
+
+      if (!trendMap[key]) {
+        trendMap[key] = 0;
+      }
+
+      if (data.type === "expense") {
+        trendMap[key] += data.amount;
+      }
+    });
+
+    const result = Object.keys(trendMap).map(key => ({
+      period: key,
+      expense: trendMap[key]
+    }));
+
+    res.json(result);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 export default router;
